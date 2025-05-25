0fcdd0910a2fee341d053598250c71a4d6bfd38f
Anshu112 <varmaanshu992@gmail.com>
2025-05-25 18:33:29 +0530

    Added SnakeGame folder with game files

---
+++ SnakeGame/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for i in range(3):
+            self.add_segment((0, 0))
+
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.snake.append(segment)
+
+    def extend(self):
+        self.add_segment(self.snake[-1].position())
+        return self.snake[-1]
+    
+
+    def move(self):
+        for seg_num in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[seg_num - 1].xcor()
+            new_y = self.snake[seg_num - 1].ycor()
+            self.snake[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+  
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+            
+           
